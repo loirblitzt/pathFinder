1a2dca865b481cd3594393d94880285e1279275f
rosseelj <rosseelj@D9020M25306.phelma.grenoble-inp.fr>
2018-05-04 13:59:33 +0200

    mise en place du projet

--- projetInfo/src/dijkstra.c
+++ projetInfo/src/dijkstra.c
@@ -1,7 +1,7 @@
 //dijkstra//
 #include "graph.h"
 #include "L_ARC.h"
-
+#include "liste.h"
 int min(int* tab,int dim){
 	int i;
 	int min=0;
@@ -21,7 +21,7 @@ L_ARC dijSolver ( T_SOMMET* G,int indDepart ,int indArrivee,int X){
 	int *pcc=calloc(X,sizeof(*pcc));
 	int *pere;
 	L_ARC S=creerarc();
-	int * C=calloc(X,sizeof(*C)); //verfier si alloc ok//
+	Liste C=calloc(X,sizeof(*C)); //verfier si alloc ok//
 	int i;
 	int ind;
 	
@@ -31,7 +31,10 @@ L_ARC dijSolver ( T_SOMMET* G,int indDepart ,int indArrivee,int X){
 		pere[i]=-1;
 	}
 	pcc[indDepart]=0;
-	do{ ind=min(pcc,X);	
+	do{ ind=min(pcc,X);
+		
+		
+			
 	    
 		
 		
